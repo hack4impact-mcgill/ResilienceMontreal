da1c05537864fba172d822c8b5e55b4cd3762329
mintary <ewang0332@gmail.com>
2025-03-26 19:41:06 -0400

    layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,7 @@
 import { type Metadata } from "next";
-import "./globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../styles/theme";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-
 
 export const metadata: Metadata = {
   title: "Dashboard",

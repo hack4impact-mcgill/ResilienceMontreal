15011de0d31fabe48971cbc85fabd1d1b2701e27
James <jamesbmadden@gmail.com>
2025-04-02 23:25:17 -0400

    redirect to login if not logged in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import Dashboard from "./Dashboard";
+import { redirectIfNotLoggedIn } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+
+  await redirectIfNotLoggedIn();
+
   return (
     <div>
       <Dashboard />

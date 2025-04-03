739c1bbfaaf5ecd77fab9c8a88f9d23be9f9a697
James <jamesbmadden@gmail.com>
2025-04-02 23:07:41 -0400

    redirect to the dashboard if the user is signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import HomePage from "@/components/HomePage";
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import jwt from "jsonwebtoken";
 
-export default function Home() {
+export default async function Home() {
+
+  // redirect to dashboard if the user is logged in
+  const cookieStore = await cookies();
+  const token = JSON.parse(cookieStore.get('token')?.value ?? "{}")?.accessToken ?? "";
+
+  // check the validity of the token
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, _decoded) => {
+    if (!err) {
+      redirect("/dashboard");
+    }
+  });
+
   return (
     <div>
       <HomePage />

feea1448a16d099ada5103a25585be0e9521e4fe
tran-ethan <tran.ethan@hotmail.com>
2025-10-08 20:17:28 -0400

    chagne login

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,15 @@
+import { TopNavbar } from "@/components/TopNavbar";
+import React from "react";
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-shrink-0">
+                <TopNavbar />
+            </div>
+            <div className="flex-1 flex items-center justify-center">
+                {children}
+            </div>
+        </div>
+    );
+}

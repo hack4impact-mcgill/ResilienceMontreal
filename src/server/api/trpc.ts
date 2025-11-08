97c76130deec131b6bed8bac14af48a0d45dd54b
mintary <ewang0332@gmail.com>
2025-11-08 01:32:31 -0500

    trpc setup

---
+++ src/server/api/trpc.ts
@@ -0,0 +1,89 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+import { db } from "~/server/db";
+import { getServerAuthSession } from "~/server/auth";
+
+// CONTEXT
+// defines the "contexts" that are available in the backend API
+// allow you to access things when processing a request, like the database, the session, etc.
+// this helper generates the "internals" for a tRPC context
+// the API handler and RSC clients each wrap this and provides the required context (https://trpc.io/docs/server/context)
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const session = await getServerAuthSession();
+
+  return {
+    db,
+    user: session.user ?? null,
+    ...opts,
+  };
+};
+
+// INITIALIZATION
+// tRPC API is initialized, connecting the context and transformer
+// ZodErrors also get parsed for typesafety on the frontend if your procedure fails due to validation errors on the backend
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+// create a server-side caller (https://trpc.io/docs/server/server-side-calls)
+export const createCallerFactory = t.createCallerFactory;
+
+// ROUTER & PROCEDURE
+// these are the pieces you use to build the tRPC API -- you should import these a lot in the "/src/server/api/routers" directory
+
+// create new routers and sub-routers in the tRPC API (https://trpc.io/docs/router)
+export const createTRPCRouter = t.router;
+
+// middleware for timing procedure execution and adding an artificial delay in dev
+// helps catch unwanted waterfalls by simulating network latency that would occur in prod but not in dev
+const timingMiddleware = t.middleware(async ({ next, path }) => {
+  const start = Date.now();
+
+  if (t._config.isDev) {
+    // artificial delay in dev
+    const waitMs = Math.floor(Math.random() * 400) + 100;
+    await new Promise((resolve) => setTimeout(resolve, waitMs));
+  }
+
+  const result = await next();
+
+  const end = Date.now();
+  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
+
+  return result;
+});
+
+// public (unauthenticated) procedure
+// base piece you use to build new queries and mutations on your tRPC API
+// does not guarantee that a user querying is authorized but you can still access user session data if theyre logged in
+export const publicProcedure = t.procedure.use(timingMiddleware);
+
+// protected (authenticated) procedure
+// if you want a query or mutation to be accessible to logged in users only
+// verifies the session is valid and guarantees 'ctx.session.user' is not null
+export const protectedProcedure = t.procedure
+  .use(timingMiddleware)
+  .use(({ ctx, next }) => {
+    // require an authenticated user (server-side user provided by getServerAuthSession)
+    if (!ctx.user) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next({
+      ctx: {
+        // downstream handlers can rely on ctx.user being present
+        user: ctx.user,
+      },
+    });
+  });

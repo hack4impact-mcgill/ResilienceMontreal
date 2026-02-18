1fb9a29bb3cedf5a870550494423c18cd2ca5e6d
Emily Wang <143962510+mintary@users.noreply.github.com>
2026-02-18 13:46:31 -0500

    remove dummy data

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -4,8 +4,8 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import { fetchClients } from "@/lib/api";
 import { redirect } from "next/navigation";
+import { api } from "~/trpc/react";
 import { getServerAuthSession } from "~/server/auth";
 import { prisma } from "@/lib/prisma";
 
@@ -27,9 +27,9 @@ export default async function ClientsPage() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ["clients"],
-    queryFn: fetchClients,
-  });
+      queryKey: ["clients"],
+      queryFn: () => api.client.list.usePrefetchQuery()
+    });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
